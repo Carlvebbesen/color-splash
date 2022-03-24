2383c7699e5d93368def20f40e18fb46a9f3fd0b
Håkon <haakon.telje@gmail.com>
2022-03-24 17:00:19 +0100

    LETS FUCKING GOO

--- backend/src/types/player.ts
+++ backend/src/types/player.ts
@@ -1,7 +1,15 @@
-interface Player {
+export interface Player {
     name : string;
-    socketID : number;
-    roomID? : string;
+    socketID : string;
+    gameID? : string;
     round? : number;
     answer? : string;
 }
+
+/**
+ * ALgoritmen for hvordan vi for hver runde emiter shit til hver individuelle player
+ * Ha en array, med players, med socketID og masse shit om hver player lagret.
+ * Itererer over den i gameloopen for hver runde, og emit til hver socket id, 
+ * dens data som finnes i dictionary players - som blir oppdatert for hver runde.
+ * 
+ */
