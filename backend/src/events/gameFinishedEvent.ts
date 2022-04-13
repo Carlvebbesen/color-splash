49a09d65e884d6b481b0e063c882edf1647200a7
Håkon <haakon.telje@gmail.com>
2022-04-13 13:38:25 +0200

    fixed some autoimport, delete game from state and emits results to room

--- backend/src/events/gameFinishedEvent.ts
+++ backend/src/events/gameFinishedEvent.ts
@@ -1,4 +1,7 @@
-import { Socket } from "socket.io";
+import { Socket, Server } from "socket.io";
+import { error, gameFinished } from "../globalEvents";
+import { deleteGame, getGame } from "../serverState";
+import { game } from "../types/internalTypes";
 
 /**
  * Server emits results to everyone in game based on gameId.
@@ -6,4 +9,14 @@ import { Socket } from "socket.io";
  * @param socket - socketId
  * @param - gameId of game that results is to be displayed
  */
-export const gameFinishedEvent = (socket: Socket, gameId: string) => {};
+export const gameFinishedEvent = (socket: Socket, io: Server,  gameId: number) => {
+    //retrieves game to be finished
+    const game: game = getGame(gameId);
+    if (!game) {socket.emit(error, "game could not be found")}
+    //Sends result array to everyone in room
+    io.in(gameId.toString()).emit(gameFinished, game.result)
+    //Makes sure to delete game from gamestate
+    deleteGame(gameId)
+    //TODO something with player array or answers?
+    //Could also emit to every player their answers
+};
